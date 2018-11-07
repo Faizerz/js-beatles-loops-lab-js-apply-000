14bd48d047fcf6b09b35a0b353b3d916e09d34ea
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2018-11-07 11:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const arry = [];
+
+  for(let x=0; x<musicians.length; x++) {
+    arry.push(musicians[x] + " plays " + instruments[x]);
+  }
+  
+  return arry;
+}
+
+
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];  
+  
+
+
+function johnLennonFacts(facts) {
+
+  const FACTS = [];
+  let y = 0;
+  
+  while(y<facts.length){
+    FACTS.push(facts[y]+'!!!');
+    y++;
+  }
+  
+  return FACTS;
+
+}
